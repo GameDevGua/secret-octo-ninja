4c55918be0dd7c815c9e5b4be344aab7192118b2
Elepole <blackhole.vs.darkhole@gmail.com>
2014-05-21 10:31:33 -0400

    Just some basic stfff

---
+++ src/com/supinfo/chess/cli/Launcher.java
@@ -0,0 +1,14 @@
+package com.supinfo.chess.cli;
+
+public class Launcher {
+
+	public Launcher() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
